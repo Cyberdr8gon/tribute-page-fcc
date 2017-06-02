9c8c069158590e28dadafe7fa28aff167df8c522
Sam Bateman <sam@bateman.io>
2017-06-01 18:16:05 -0600

    finished project v1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,5 +12,31 @@ jQuery(document).ready(function($) {
 	        window.location.hash = target;
 	    });
 	});
+    let width = 0;
+    let height = 0;
+  if(window.screen.width > 560) {
+    width = 560;
+    height = 315;
+  } else if (window.screen.width > 380) {
+    width = 380;
+    height = 214;
+  } else {
+    width = 300;
+    height = 168.75;
+  }
+
+  $('.video-container').html(`
+       <iframe width="${width}" 
+               height="${height}" 
+               src="https://www.youtube-nocookie.com/embed/o4SGkB_8fFs" 
+               frameborder="0" 
+               allowfullscreen>
+               </iframe>
+
+       `)
+
+  $('#copyright-current-year').html(new Date().getFullYear());
   
 });
+
+
